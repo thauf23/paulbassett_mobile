9b15c9361a35b26cc0374dc5cd455d26bd61bd98
shinil_project <thauf23@naver.com>
2020-07-23 18:47:20 +0900

    js start

--- style/js/menu.js
+++ style/js/menu.js
@@ -1,3 +1,18 @@
 $(function(){
+    //start
+
+    const menuIcon = document.querySelector(".menu_icon");
+    const mainMenu = document.querySelector(".main_menu");
+
+    //menu open
+    menuIcon.addEventListener("click", function(){
+        mainMenu.style.left = 0;
+    });
     
+    //menu close
+
+
+    
+    console.log(mainMenu);
+    //end
 });
