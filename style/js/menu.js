953d766da90c53e78083d60e66efd84f940060fe
shinil_project <thauf23@naver.com>
2020-07-23 19:08:15 +0900

    menu close

--- style/js/menu.js
+++ style/js/menu.js
@@ -3,16 +3,21 @@ $(function(){
 
     const menuIcon = document.querySelector(".menu_icon");
     const mainMenu = document.querySelector(".main_menu");
+    const bodyBackground = document.querySelector(".body_background");
 
     //menu open
     menuIcon.addEventListener("click", function(){
         mainMenu.style.left = 0;
+        bodyBackground.style.zIndex = 90;
     });
     
     //menu close
-
+    bodyBackground.addEventListener("click", function(){
+        mainMenu.style.left = "-50%";
+        bodyBackground.style.zIndex = "-1";
+    });
 
     
-    console.log(mainMenu);
+    // console.log("a");
     //end
 });
